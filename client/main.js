7f07e966b26ca03853ac69a16144cbc2bc6b50b6
michau_k <alex.michaux@epitech.eu>
2016-02-08 14:58:22 -0500

    Starting client: opening a window with electron

---
+++ client/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+});
